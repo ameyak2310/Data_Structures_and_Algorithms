c74596a5ce6f6563977fefe2c96187ffa18b6548
Ameya Kulkarni <35555732+ameyak2310@users.noreply.github.com>
2023-06-09 15:46:21 +0530

    Factorial iteratively

--- src/Recursion_Factorial.py
+++ src/Recursion_Factorial.py
@@ -1,11 +1,50 @@
-def calcFactorial(n):
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 1
+"""
+Calculates Factorial 
+"""
+import time
+
+def factorial_recur(number):
+    """Calculate facorial of an interger recursively
+    Args:
+        n (int): Integer
+    Returns:
+        result: returns factorial of Interger n
+    """
+    if number == 0:
+        result = 1
+    elif number == 1:
+        result = 1
     else:
-        return n*calcFactorial(n-1)
-    
-print('Output\n')    
-print(calcFactorial(5))
-    
+        result = number*factorial_recur(number-1)
+    return result
+
+def factorial_iter(number):
+    """Calculate facorial of an interger recursively
+    Args:
+        n (int): Integer
+    Returns:
+        result: returns factorial of Interger n
+    """
+    if number == 0:
+        result = 1
+    elif number == 1:
+        result = 1
+    else:
+        result = 1
+        for i in range(1,number+1):
+            result = result * i
+    return result
+
+NUMBER = 10
+
+print("\n___OUTPUT with Recursion___")
+start_time = time.time()
+print(f"Factorial of {NUMBER} : {factorial_recur(NUMBER)}")
+end_time = time.time()
+print(f"Compute time : {end_time-start_time}\n")
+
+print("\n___OUTPUT without Recursion___")
+start_time = time.time()
+print(f"Factorial of {NUMBER} : {factorial_iter(NUMBER)}")
+end_time = time.time()
+print(f"Compute time : {end_time-start_time}\n")
