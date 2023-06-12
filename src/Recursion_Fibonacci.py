7a81e4738484e8a922d70d8ccd275817165157f3
Ameya Kulkarni <35555732+ameyak2310@users.noreply.github.com>
2023-06-12 11:25:54 +0530

    update fiblist

--- src/Recursion_Fibonacci.py
+++ src/Recursion_Fibonacci.py
@@ -1,43 +1,50 @@
-""" 
+"""
 Function to return Fibonacci Series
 """
 #%% Last fibonacci Element
 
 
-def fib(n):
+def fib(num):
     """Return Fibonacci of an interger n
     Args:
-        n (int): input 
+        n (int): input
     Returns:
         result(int): Fibonacci of n
     """
-    if n == 0 or n == 1:
+    if num in (0,1):
         result = 1
     else:
-        result = fib(n-1) + fib(n-2)
+        result = fib(num - 1) + fib(num - 2)
     return result
 
 
-#%% Function call return number          
+#%% Function call return number
 NUM = 10
-print("\n___OUTPUT___")
+print("\n******************************")
 for NUM in range(NUM):
-    print(f'Fibonacci of {NUM} is {fib(NUM)}')
-    
+    print(f"Fibonacci of {NUM} is {fib(NUM)}")
+print("\n******************************")
+
 #%% Returns Fibonacci series
 
+def fib_series(num):
+    """Returns fibonacci list of n element
 
-def fib_series(n):
-    fib_list = [1] * n + [1]
-    fib_list[0] = 0
-    for i in range(3, n+1):
-        fib_list[i] = fib_list[i-1] + fib_list[i-2]
-        
+    Args:
+        n (int): fibonacci list of len(n) will be generated
+
+    Returns:
+        fib_list(list): Fibonacci List
+    """
+    fib_list = [0] + [1] * num
+    for i in range(3, num + 1):
+        fib_list[i] = fib_list[i - 1] + fib_list[i - 2]
+
     return fib_list
-       
+
 
 #%% Function call : Return List
-PAYLOAD = 100
+PAYLOAD = 10
 func = fib_series
 
 print("\n******************************")
