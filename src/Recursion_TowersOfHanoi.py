0e80bb8009b2aeab10b52fb65abf65558c8d2ed9
Ameya Kulkarni <ameya2310@gmail.com>
2023-06-13 22:11:26 +0530

    lint

--- src/Recursion_TowersOfHanoi.py
+++ src/Recursion_TowersOfHanoi.py
@@ -3,7 +3,8 @@ Towers of Hanoi
 Demonstarion of Recursion
 """
 
-#%% Function
+# %% Function
+
 
 def hanoi(disks=3, from_tower=1, to_tower=3, alt_tower=2):
     """Prints out steps to move Towers
@@ -15,22 +16,18 @@ def hanoi(disks=3, from_tower=1, to_tower=3, alt_tower=2):
         alt_tower (int, optional): Spare tower. Defaults to 2.
     """
     if disks != 0:
-        hanoi(disks-1, from_tower, alt_tower, to_tower)
-        print('Move disk {} from tower {} to tower {}'.format(disks, from_tower, to_tower ))
-        hanoi(disks-1, alt_tower, to_tower, from_tower)
+        hanoi(disks - 1, from_tower, alt_tower, to_tower)
+        print(f"Move disk {disks} from tower {from_tower} to tower {to_tower}")
+        hanoi(disks - 1, alt_tower, to_tower, from_tower)
 
 
-#%% Function Call
+# %% Function Call
 
 DISKS = 3
 FROM_TOWER = 1
 TO_TOWER = 3
 SPARE_TOWER = 2
 
-hanoi(disks = DISKS,
-    from_tower = FROM_TOWER,
-    to_tower= TO_TOWER,
-    alt_tower= SPARE_TOWER
-      )
+hanoi(disks=DISKS, from_tower=FROM_TOWER, to_tower=TO_TOWER, alt_tower=SPARE_TOWER)
 
 # %%
