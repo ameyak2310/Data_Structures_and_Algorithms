083245b0bde28795120109ba293f9b1901d4b28c
Ameya Kulkarni <ameya2310@gmail.com>
2023-06-16 18:34:08 +0530

    Added print and prompt functionality

---
+++ src/Oops_PhoneBook.py
@@ -0,0 +1,89 @@
+""" 
+Phonebook
+"""
+# %%
+import datetime
+import pandas as pd
+
+
+class Contact:
+    """Class Contact to store contents of Phonebook
+
+    Returns:
+        None: Performs functions as defined in main app
+    """
+
+    databook = {
+        "NAME": [],  # ["Default"],
+        "NUMBER": [],  # [00000],
+        "EMAIL": [],  # ["default@default.com"],
+        "UPDATE": [],  # ["2023-06-16 17:13:28.161998"],
+    }
+
+    def __init__(self, name, number, email) -> None:
+        self.name = name
+        self.number = number
+        self.email = email
+        self.update = str(datetime.datetime.now())
+
+    def add() -> None:
+        """Adds contact to databook"""
+        name = input("Enter contact name : ")
+        number = input("Enter contact number : ")
+        email = input("Enter contact email : ")
+        contact = Contact(name, number, email)
+        Contact.databook["NAME"].append(contact.name)
+        Contact.databook["NUMBER"].append(contact.number)
+        Contact.databook["EMAIL"].append(contact.email)
+        Contact.databook["UPDATE"].append(str(datetime.datetime.now()))
+
+    def display() -> None:
+        """Returns current databook into a dataframe
+
+        Returns:
+            df(DataFrame): Current databook
+        """
+        df = pd.DataFrame(
+            data=Contact.databook, columns=["NAME", "NUMBER", "EMAIL", "UPDATE"]
+        )
+        return df
+
+    def prompt() -> None:
+        """Prompts user for input
+
+        Returns:
+            letter(str): p, a, d, s, u
+        """
+        print(
+            "\nAvailable Functionality : \n",
+            "p: Print Contacts | a: Add | d: Delete | s: Search | u: Update | q: Quit",
+        )
+        return input("Enter function: ")
+
+
+# %%
+def main():
+    """Main function for Phonebook"""
+    print("\n\n>>> Phone Book <<<")
+    print(f"No of contacts : {Contact.display().shape[0]}")
+    status = True
+    while status:
+        function = Contact.prompt()
+
+        if function == "p":
+            print("\n Current Phonebook")
+            print("\n\n--------------------")
+            print(Contact.display(), "\n")
+            print("--------------------\n\n")
+
+        elif function == "a":
+            Contact.add()
+            print("Contact added succesfully")
+
+        elif function == "q":
+            status = False
+
+
+# %% Main App Call
+if __name__ == "__main__":
+    main()
